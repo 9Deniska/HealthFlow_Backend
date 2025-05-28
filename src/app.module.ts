d37464d9d94cc5f71a44b03571be523a90b226ef
Deniska <denys.mykhailenko@nure.ua>
2025-05-28 19:44:30 +0300

    init

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,36 +1,24 @@
-// src/app.module.ts
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config'; // Import ConfigService
-import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm'; // Import TypeOrmModuleOptions
-import { AppController } from './app.controller'; // Assuming AppController exists
-import { AppService } from './app.service'; // Assuming AppService exists
-import { AuthModule } from './auth/auth.module'; // Assuming AuthModule exists and is needed
-import { User } from './users/dto/user.entity'; // Import the User entity
-import { UsersModule } from './users/users.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      // Configure ConfigModule
-      isGlobal: true, // Make ConfigModule available globally
-      envFilePath: '.env', // Specify the .env file path
-    }),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule], // Make ConfigService available
-      inject: [ConfigService], // Inject ConfigService
-      useFactory: async (
-        configService: ConfigService,
-      ): Promise<TypeOrmModuleOptions> => ({
-        type: 'mysql',
-        url: configService.get<string>('DATABASE_URL'),
-        entities: [User], // Use the imported User entity directly
-        synchronize: true,
-      }),
-    }),
-    UsersModule,
-    AuthModule, // Ensure AuthModule is correctly imported if it's separate
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      autoLoadEntities: true,
+      synchronize: true,
+    })
   ],
-  controllers: [AppController], // Add your main app controller
-  providers: [AppService], // Add your main app service
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
