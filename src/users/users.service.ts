4165c7e2d48e7fa9838b6b208db2f7c4c8f8eb0b
Deniska <denys.mykhailenko@nure.ua>
2025-05-30 02:46:00 +0300

    Added Patch Profile

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -4,6 +4,7 @@ import * as bcrypt from 'bcrypt';
 import { Repository } from 'typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './entities/user.entity';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
 export class UsersService {
@@ -32,5 +33,26 @@ async findByPhone(phone: string) {
 async findById(id: number): Promise<User | null> {
   return this.usersRepo.findOne({ where: { user_id: id } });
 }
+
+ async update(userId: number, dto: UpdateUserDto): Promise<User> {
+    const user = await this.usersRepo.findOne({ where: { user_id: userId } });
+
+    if (!user) {
+      throw new Error('User not found');
+    }
+
+    if (dto.password) {
+      user.password_hash = await bcrypt.hash(dto.password, 10);
+    }
+
+    if (dto.name !== undefined) user.name = dto.name;
+    if (dto.surname !== undefined) user.surname = dto.surname;
+    if (dto.middlename !== undefined) user.middlename = dto.middlename;
+    if (dto.date_of_birth !== undefined) user.date_of_birth = new Date(dto.date_of_birth);
+    if (dto.email !== undefined) user.email = dto.email;
+    if (dto.phone !== undefined) user.phone = dto.phone;
+
+    return this.usersRepo.save(user);
+  }
 
 }
