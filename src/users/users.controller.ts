fa3871eebe1fd212440c87c071d62bcffce964e6
Deniska <denys.mykhailenko@nure.ua>
2025-05-29 13:00:23 +0300

    added getProfile

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,13 +1,17 @@
-import { Body, Controller, Post } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { JwtAuthGuard } from '../auth/guard/jwt-auth.guard';
+import { RequestWithUser } from '../common/interfaces/request-with-user.interface';
+import { Req } from '@nestjs/common';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
-  @Post()
-  async create(@Body() dto: CreateUserDto) {
-    return this.usersService.create(dto);
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  async getProfile(@Req() req: RequestWithUser) {
+    const userId = req.user.sub;
+    return this.usersService.findById(userId);
   }
 }
